014c9f46e23076a839f046c5cc5a65a842ecb9fd
DSP Rookie <sraman.iisc@gmail.com>
2025-04-19 07:26:05 +0000

    Adds Makefile and assembly code to add in a loop

--- m.s
+++ m.s
@@ -1,2 +1,14 @@
+# Any thing starting with a # is a comment and is ignored by the assembler
+# Label : Any word ending with a : is a label
+# directives : anything staing with .
 _start:
-    j _start
+    addi x1, x0, 2
+    addi x2, x0, 5
+    addi x3, x0, 0
+    j addLoop
+    j .
+    
+addLoop:
+    add x3, x3, x1
+    addi x2, x2, -1
+    bnez x2, addLoop
